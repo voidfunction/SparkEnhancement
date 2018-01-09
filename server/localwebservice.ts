e1c9f3aceaf8b6bb02d580fdf257b8b324072b9a
djlan <djlan@microsoft.com>
2018-01-09 18:47:43 +0800

    Add log tab

--- server/localwebservice.ts
+++ server/localwebservice.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 var app = express();
 let rootPath = path.join(__dirname, '..', '..');
 console.log('Local web resources root path: ' + rootPath);
-app.use("/", express.static(rootPath));
+app.use("/datatab", express.static(path.join(rootPath, "/datatab")));
+app.use("/logtab", express.static(path.join(rootPath, "/logtab")));
 
 app.listen(5555, function() { console.log("http://localhost:5555 is running");  });
