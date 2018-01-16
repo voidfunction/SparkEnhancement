3e26b21ca76f355f37564a7a253e72c160aefd56
ltian <ltian@microsoft.com>
2018-01-16 14:25:00 +0800

    add featue of download file

--- datatab/index.ts
+++ datatab/index.ts
@@ -94,8 +94,9 @@ let isRendered: boolean = false;
 
 window.addEventListener("message", (event) => {
     console.log(event);
-    if (event.data.data) {
-        spark.data = event.data.data;
+    if (event.data.inputAndOutputs) {
+        spark.data = event.data.inputAndOutputs.data;
+        spark.parentOrigin = event.data.source;
         renderInputAndOutput();
     }
 });
@@ -113,38 +114,73 @@ function postIframeReadyMessage() {
     window.parent.postMessage(message, '*');
 }
 
-
 function renderInputAndOutput() {
     renderData(spark.data.inputs);
     renderData(spark.data.outputs, false);
-    addButtonClickEvent();
+    addTableButtonClickEvent();
 }
 
-function addButtonClickEvent() {
-    $('.data-tab-button').click((event) => {
-        const actionType: DataTabButtonTye = getActionEnumType(event.target.id);
+function addTableButtonClickEvent() {
+    $(".full-path").click((event) => {
+        copyPathToClipboard(true, event);
+    });
+
+    $(".releative-path").click((event) => {
+        copyPathToClipboard(false, event);
+    });
+}
+
+interface TableElementAction {
+    (path: string, format: string, ...args: any[]): void;
+}
+
+const blobReleatviePathRexgex = new RegExp("[^:]*://[^/]*@[^/]*/(.*)");
+
+function tableElementAction(event, action: TableElementAction, ...args: any[]) {
         // find the row content
         const parentRowDom = $(event.target).parentsUntil('tbody');
-        if (parentRowDom.length !== 3) {
-            alert('incorrect row counter!');
-            return;
+        const columns = $(parentRowDom).children('td');
+        const [inputSetId, format, path] = [columns[0].innerHTML, columns[1].innerHTML, columns[2].innerHTML];
+        if (action) {
+            action(path, format, args);
         }
-        let columns = $(parentRowDom).children('td');
-        let [inputSetId, format, path] = [columns[0].innerHTML, columns[1].innerHTML, columns[2].innerHTML];
-    });
+}
+
+function copyPathToClipboard(isFullPath: boolean = true, event) {
+        // find the row content
+        tableElementAction(event, (path: string, format: string) => {
+            if (isFullPath) {
+                copyToClipboard(path);
+            } else {
+                const releativePath = path.match(blobReleatviePathRexgex)[1];
+                copyToClipboard(releativePath);
+            }
+        });
+}
+
+function copyToClipboard(content: string) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val(content).select();
+  document.execCommand("copy");
+  $temp.remove();
 }
 
 function renderData(dataSet: Array<Array<string>>, isInput: boolean = true) {
     dataSet && dataSet.forEach(elements => {
+        const fullPath = elements[2].trim();
+        const format = elements[1].trim().toLowerCase();
+        const fileName = fullPath.substring(fullPath.lastIndexOf('/') +　1);
+        let downloadPath = `${spark.parentOrigin}/history/data?url=${encodeURIComponent(fullPath)}&format=json&rowCount=100`;
         elements.push(`
         <div class="btn-group">
-            <button type="button" id="download" class="btn btn-default data-tab-button">Download</button>
+            <button type="button" class="download btn btn-default data-tab-button"><a href="${downloadPath}" download="${fileName}">download</a></button>
             <div class="btn-group">
                 <button type="button" class="data-tab-button btn btn-default dropdown-toggle" data-toggle="dropdown">...<span class="caret"></span></button>
                 <ul class="dropdown-menu" role="menu">
-                <li><button id="preview" class="btn btn-md btn-default data-tab-button">Preview</button></li>
-                <li><button id="fullPath" class="btn btn-md btn-default data-tab-button">Copy Full Path</button></li>
-                <li><button id="releativePath" class="btn btn-md btn-default data-tab-button">Copy Releative Path</button></li>
+                <li><button class="preview-button btn btn-md btn-default data-tab-button">Preview</button></li>
+                <li><button class="full-path btn btn-md btn-default data-tab-button">Copy Full Path</button></li>
+                <li><button class="releative-path btn btn-md btn-default data-tab-button">Copy Releative Path</button></li>
                 </ul>
             </div>
         </div>
